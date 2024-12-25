04b8c12f1d0205da2ffd6bafccd94381eaf1dcec
gaffar <gaffarshaikh07@gmail.com>
2024-12-25 16:46:36 +0530

    EOD 16:46

--- src/test/java/comm/demo.java
+++ src/test/java/comm/demo.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class demo {
@@ -11,12 +13,16 @@ public class demo {
 	
 	@Test
 	public void test() {
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();  
+		options.addArguments("--headless");
+		WebDriver driver=new ChromeDriver(options);
         
 		driver.manage().window().maximize();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.getTitle();
+        Reporter.log(driver.getTitle(),true);
+        Reporter.log("hello Gaffar",true);
+        
 	
 }
 
